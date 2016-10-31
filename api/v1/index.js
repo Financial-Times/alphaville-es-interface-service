21c6eca591f26a6aa3d93e26fc04f889e3ecc375
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2016-10-31 18:46:52 +0200

    Add author api route

--- api/v1/index.js
+++ api/v1/index.js
@@ -1,13 +1,13 @@
+const _ = require('lodash');
 const router = require('express').Router();
 const es = require('alphaville-es-interface');
 const vanityRegex = /^\/article(\/[0-9]+\/[0-9]+\/[0-9]+\/[0-9]+\/.*)$/;
 const uuidRegex = /^\/article\/([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})$/;
 
-router.get('/articles', (req, res) => {
-	let offset = req.query.offset || 0;
-	let limit = req.query.limit || 30;
-
-	es.searchArticles({
+const getEsQueryForArticles = (req) => {
+	let offset = parseInt(req.query.offset, 10) || 0;
+	let limit = parseInt(req.query.limit, 10) || 30;
+	return {
 		sort: {
 			publishedDate: {
 				order: 'desc'
@@ -15,12 +15,33 @@ router.get('/articles', (req, res) => {
 		},
 		from: offset,
 		size: limit
-	})
-	.then(articles => res.json(articles))
-	.catch(console.log);
+	};
+};
+
+//articles
+router.get('/articles', (req, res, next) => {
+	const searchString = req.query.q || null;
+	let esQuery = getEsQueryForArticles(req);
+
+	if (searchString) {
+		let searchQuery = {
+			query: {
+				multi_match: {
+					query: searchString,
+					fields: ["titles", "byline"]
+				}
+			}
+		};
+		esQuery =  _.merge(esQuery, searchQuery);
+	}
+
+	es.searchArticles(esQuery)
+		.then(articles => res.json(articles))
+		.catch(next);
 });
 
-router.get(vanityRegex,  (req, res) => {
+//article
+router.get(vanityRegex,  (req, res, next) => {
 	let urlToSearch = req.params[0];
 	if (urlToSearch[0] === '/') {
 		urlToSearch = urlToSearch.substr(1, urlToSearch.length);
@@ -33,14 +54,32 @@ router.get(vanityRegex,  (req, res) => {
 
 	return es.getArticleByUrl(urlToSearch)
 		.then(article => res.json(article))
-		.catch(console.log);
+		.catch(next);
 });
 
-
-router.get(uuidRegex, (req, res) => {
+router.get(uuidRegex, (req, res, next) => {
 	return es.getArticleByUuid(req.params[0])
 		.then(article => res.json(article))
-		.catch(console.log)
+		.catch(next)
+});
+
+//author
+router.get('/author', (req, res, next) => {
+	const authorString = req.query.q || null;
+	let esQuery = getEsQueryForArticles(req);
+	if (authorString) {
+		let authorQuery = {
+			query: {
+				wildcard : {
+					byline: `*${authorString}*`
+				}
+			}
+		};
+		esQuery = _.merge(esQuery, authorQuery);
+	}
+	es.searchArticles(esQuery)
+		.then(articles => res.json(articles))
+		.catch(next);
 });
 
 module.exports = router;
