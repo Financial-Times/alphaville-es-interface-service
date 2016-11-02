47bf0f5a622e3872ceb2d9ab29dc3cb742797378
Roland Vachter <roland.vachter@iquestgroup.com>
2016-11-02 11:12:21 +0200

    Return true for gtg in sign that the application is up.

--- appSetup.js
+++ appSetup.js
@@ -32,18 +32,7 @@ module.exports = (app, config) => {
 		},
 		goodToGoTest: function() {
 			return new Promise(function(resolve) {
-				healthcheck.getChecks().then(checks => {
-					let ok = true;
-					checks.forEach(check => {
-						if (check.ok !== true) {
-							ok = false;
-						}
-					});
-
-					resolve(ok);
-				}).catch(() => {
-					resolve(false);
-				});
+				resolve(true);
 			});
 		},
 		healthCheck: function() {
