7e69b61d30e0ba91b6914fb5e6e35efba07f18a9
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2017-06-27 17:16:58 +0300

    Update the temp fix for webUrl

--- api/v2/index.js
+++ api/v2/index.js
@@ -1,6 +1,5 @@
 const _ = require('lodash');
 const router = new (require('express')).Router();
-const moment = require('moment-timezone');
 const es = require('../../es/v2/main');
 const suds = require('../../services/suds');
 const fastly = require('../../services/fastly');
@@ -313,10 +312,6 @@ router.get('/marketslive', (req, res, next) => {
 			if (response) {
 				let isLive = false;
 				response.items.forEach(article => {
-					if (article.webUrl.indexOf('marketslive') === -1) {
-						let mlDate = moment(article.firstPublishedDate).format('YYYY-MM-DD');
-						article.webUrl = `http://ftalphaville.ft.com/marketslive/${mlDate}/`
-					}
 					if (article.isLive) {
 						isLive = true;
 					}
