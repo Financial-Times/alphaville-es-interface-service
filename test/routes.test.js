39e59780d71ee78000612b0f751c6040f1d5220a
AniaMakes <anna.bebb@ft.com>
2020-04-22 12:15:38 +0100

    updated tests to function with the v2 api

--- test/routes.test.js
+++ test/routes.test.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const request = require('supertest');
 const expect = require('expect');
-const app = require('../server').startServer({id: 1});
+const app = require('../server').startServer({ id: 1 });
 
 
 describe('api routes', () => {
@@ -17,10 +17,10 @@ describe('api routes', () => {
 		});
 	});
 
-	describe('GET /v1.*', () => {
+	describe('GET /v2.*', () => {
 		it('should require an api key', (done) => {
 			request(app)
-				.get('/v1')
+				.get('/v2')
 				.expect(401)
 				.end((err, res) => {
 					expect(res.text).toBe('Unauthorized');
@@ -28,52 +28,52 @@ describe('api routes', () => {
 				});
 		});
 	});
-	describe('GET /v1/articles', () => {
+	describe('GET /v2/articles', () => {
 		it('should return one article when limit is set to 1', (done) => {
 			request(app)
-				.get('/v1/articles?limit=1')
+				.get('/v2/articles?limit=1')
 				.set('X-API-KEY', process.env['API_KEY'])
 				.expect(200)
 				.end((err, res) => {
-					expect(res.body).toBeAn(Array);
-					expect(res.body.length).toBe(1);
+					expect(res.body.items).toBeAn(Array);
+					expect(res.body.items.length).toBe(1);
 					done();
 				});
 		});
 		it('should return one article when limit is set to 1 and search term is set', (done) => {
 			request(app)
-				.get(`/v1/articles?q=${encodeURIComponent('Paul Murphy')}&limit=1`)
+				.get(`/v2/articles?q=${encodeURIComponent('Paul Murphy')}&limit=1`)
 				.set('X-API-KEY', process.env['API_KEY'])
 				.expect(200)
 				.end((err, res) => {
-					expect(res.body).toBeAn(Array);
-					expect(res.body.length).toBe(1);
+					expect(res.body.items).toBeAn(Array);
+					expect(res.body.items.length).toBe(1);
 					done();
 				});
 		});
 	});
-	describe('GET /v1/author', () => {
+	describe('GET /v2/author', () => {
 		it('should return one article by the searched author when limit is set to 1', (done) => {
 			let author = 'Paul Murphy';
 			request(app)
-				.get(`/v1/articles?q=${encodeURIComponent(author)}&limit=1`)
+				.get(`/v2/author?q=${encodeURIComponent(author)}&limit=1`)
 				.set('X-API-KEY', process.env['API_KEY'])
 				.expect(200)
 				.end((err, res) => {
-					expect(res.body).toBeAn(Array);
-					expect(res.body.length).toBe(1);
-					expect(res.body[0].byline).toEqual(author);
+					expect(res.body.items).toBeAn(Array);
+					expect(res.body.items.length).toBe(1);
+					expect(res.body.items[0].byline).toEqual(author);
 					done();
 				});
 		});
 	});
-	describe('GET /v1/article', () => {
+	describe('GET /v2/article', () => {
 		before(() => {
 			let articleId = null;
 		});
 		it('should return one article by vanity', (done) => {
 			request(app)
-				.get(`/v1/article/${process.env['TEST_ARTICLE_VANITY']}`)
+				.get(`/v2/article/${process.env['TEST_ARTICLE_VANITY']}`)
 				.set('X-API-KEY', process.env['API_KEY'])
 				.expect(200)
 				.end((err, res) => {
@@ -84,7 +84,7 @@ describe('api routes', () => {
 		});
 		it('should return the same article by uuid', (done) => {
 			request(app)
-				.get(`/v1/article/${articleId}`)
+				.get(`/v2/article/${articleId}`)
 				.set('X-API-KEY', process.env['API_KEY'])
 				.expect(200)
 				.end((err, res) => {
